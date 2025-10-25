f282f164b1ad0922e1d17caccdae9bbe4dae357f
paria <pariyataba@gmail.com>
2025-10-25 18:44:07 +0200

    set up /post for users

--- srcServer/routes/userRouter.ts
+++ srcServer/routes/userRouter.ts
@@ -1,11 +1,15 @@
 import express from "express";
-import { ScanCommand } from "@aws-sdk/lib-dynamodb";
+import { PutCommand, ScanCommand } from "@aws-sdk/lib-dynamodb";
 import { ddbDocClient } from "../data/dynamodb.js";
 import type { Response,Request } from "express";
+import { createUserSchema } from "../validering/userValidate.js";
+import bcrypt from "bcrypt"
 
 const router = express.Router();
+router.use(express.json()); 
+ 
 
-
+//GET /users
 router.get("/", async (req:Request, res:Response) => {
   try {
 
@@ -24,4 +28,43 @@ const params = {
   }
 });
 
+//POST /users
+router.post("/", async (req: Request, res: Response) => {
+  try {
+
+	const parsed = createUserSchema.safeParse(req.body);
+    if (!parsed.success) {
+      return res.status(400).json({ message:"User validate has problem"});
+    }
+
+    const { userName, email, password } = parsed.data;
+
+    // Hash the password
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    // Create PK/SK
+    const pk = `USER#${Date.now()}`;
+    const sk = "PROFILE";
+    const createdAt = new Date().toISOString();
+
+    const params = {
+      TableName: "chappy",
+      Item: {
+        pk,
+        sk,
+        userName,
+        email,
+        passwordHash,
+        createdAt
+      }
+    };
+
+    await ddbDocClient.send(new PutCommand(params));
+
+    res.status(201).json({ message: "User created", user: { pk,sk, userName, email,passwordHash,createdAt } });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Could not create user" });
+  }
+});
 export default router;
