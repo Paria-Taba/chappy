16136278a1b2e8b909ac98fb4c288dff32fabebd
paria <pariyataba@gmail.com>
2025-11-12 10:39:30 +0100

    updated code after demonstration and feedback

--- srcServer/routes/userRouter.ts
+++ srcServer/routes/userRouter.ts
@@ -88,7 +88,7 @@ router.post("/login", async (req, res) => {
   );
 
   const user = data.Items?.[0];
-  if (!user) return res.status(404).json({ error: "User not found" });
+  if (!user) return res.status(404).json({ error: "Invalid username or password" });
 
   const match = await bcrypt.compare(password, user.passwordHash);
   if (!match) return res.status(401).json({ error: "Incorrect password" });
